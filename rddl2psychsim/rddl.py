08d7b64fd56f961f1a9f532ed5f041c3c0f741fc
Pedro Sequeira <pedrodbs@gmail.com>
2021-04-05 21:49:08 -0700

    added option to parse RDDL directly from string (useful for testing

--- rddl2psychsim/rddl.py
+++ rddl2psychsim/rddl.py
@@ -5,11 +5,15 @@ __author__ = 'Pedro Sequeira'
 __email__ = 'pedrodbs@gmail.com'
 
 
-def parse_rddl(rddl_file: str, verbose: bool) -> RDDL:
+def parse_rddl_file(rddl_file: str, verbose: bool) -> RDDL:
     # read RDDL file
     with open(rddl_file, 'r') as file:
         rddl = file.read()
 
+    return parse_rddl(rddl, verbose)
+
+
+def parse_rddl(rddl: str, verbose: bool) -> RDDL:
     # parse RDDL
     rddl_parser = RDDLParser(verbose=verbose)
     # rddl_parser.debugging = verbose
