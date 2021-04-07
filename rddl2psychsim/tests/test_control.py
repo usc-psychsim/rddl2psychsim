43ea86e2ce83c75825c41a9c4d7d0a11a667d316
Pedro Sequeira <pedrodbs@gmail.com>
2021-04-07 09:53:15 -0700

    added more unit tests for switch and if statement cases

--- rddl2psychsim/tests/test_control.py
+++ rddl2psychsim/tests/test_control.py
@@ -83,6 +83,40 @@ class TestRelational(unittest.TestCase):
         p = conv.world.getState(AG_NAME, 'p', unique=True)
         self.assertEqual(p, q if (q > r) else r)
 
+    def test_if_nested(self):
+        rddl = '''
+                domain my_test {
+                    pvariables { 
+                        p : { state-fluent,  int, default = -1 };
+                        q : { state-fluent,  int, default = 5 };
+                        r : { state-fluent,  int, default = 4 };
+                        s : { state-fluent,  int, default = 6 };
+                        a : { action-fluent, bool, default = false }; 
+                    };
+                    cpfs { p' = if (q > r) then if (q > s) then q else 0 else -1; };
+                    reward = 0;
+                }
+                non-fluents my_test_empty { domain = my_test; }
+                instance my_test_inst { domain = my_test; init-state { a; }; }
+                '''
+        conv = Converter()
+        conv.convert_str(rddl, AG_NAME)
+        p = conv.world.getState(AG_NAME, 'p', unique=True)
+        q = conv.world.getState(AG_NAME, 'q', unique=True)
+        r = conv.world.getState(AG_NAME, 'r', unique=True)
+        s = conv.world.getState(AG_NAME, 's', unique=True)
+        self.assertEqual(p, -1)
+        conv.world.step()
+        p = conv.world.getState(AG_NAME, 'p', unique=True)
+        if q > r:
+            if q > s:
+                p_ = q
+            else:
+                p_ = 0
+        else:
+            p_ = -1
+        self.assertEqual(p, p_)
+
     def test_if_const(self):
         rddl = '''
                 domain my_test {
@@ -103,6 +137,275 @@ class TestRelational(unittest.TestCase):
         conv.world.step()
         p = conv.world.getState(AG_NAME, 'p', unique=True)
         self.assertEqual(p, 6 if (2 != 4) else 3)
+
+    def test_switch_vars(self):
+        rddl = '''
+                domain my_test {
+                    pvariables { 
+                        p : { state-fluent,  int, default = 0 };
+                        q : { state-fluent,  int, default = 1 };
+                        r : { state-fluent,  int, default = 2 };
+                        s : { state-fluent,  int, default = 3 };
+                        a : { action-fluent, bool, default = false }; 
+                    };
+                    cpfs { p' = switch (p) {
+                                    case q : q,
+                                    case r : r,
+                                    case s : s,
+                                    default: 4
+                                }; 
+                    };
+                    reward = 0;
+                }
+                non-fluents my_test_empty { domain = my_test; }
+                instance my_test_inst { domain = my_test; init-state { a; }; }
+                '''
+        conv = Converter()
+        conv.convert_str(rddl, AG_NAME)
+        _p = conv.world.getState(AG_NAME, 'p', unique=True)
+        self.assertEqual(_p, 0)
+        conv.world.step()
+        p = conv.world.getState(AG_NAME, 'p', unique=True)
+        q = conv.world.getState(AG_NAME, 'q', unique=True)
+        r = conv.world.getState(AG_NAME, 'r', unique=True)
+        s = conv.world.getState(AG_NAME, 's', unique=True)
+        self.assertEqual(p, _python_switch(_p, {q: q,
+                                                r: r,
+                                                s: s,
+                                                'default': 4}))
+
+    def test_switch_nested_if(self):
+        rddl = '''
+                domain my_test {
+                    pvariables { 
+                        p : { state-fluent,  int, default = 1 };
+                        q : { state-fluent,  int, default = 1 };
+                        r : { state-fluent,  int, default = 4 };
+                        a : { action-fluent, bool, default = false }; 
+                    };
+                    cpfs { p' = switch (p) {
+                                    case q : if (q > r) then q else r,
+                                    case r: r,
+                                    default: 4
+                                }; 
+                    };
+                    reward = 0;
+                }
+                non-fluents my_test_empty { domain = my_test; }
+                instance my_test_inst { domain = my_test; init-state { a; }; }
+                '''
+        conv = Converter()
+        conv.convert_str(rddl, AG_NAME)
+        _p = conv.world.getState(AG_NAME, 'p', unique=True)
+        self.assertEqual(_p, 1)
+        conv.world.step()
+        p = conv.world.getState(AG_NAME, 'p', unique=True)
+        q = conv.world.getState(AG_NAME, 'q', unique=True)
+        r = conv.world.getState(AG_NAME, 'r', unique=True)
+        self.assertEqual(p, _python_switch(_p, {q: q if (q > r) else r,
+                                                r: r,
+                                                'default': 4}))
+
+    def test_switch_consts(self):
+        rddl = '''
+                domain my_test {
+                    pvariables { 
+                        p : { state-fluent,  int, default = 0 };
+                        a : { action-fluent, bool, default = false }; 
+                    };
+                    cpfs { p' = switch (p) {
+                                    case 1 : 1,
+                                    case 2 : 2,
+                                    case 3 : 3,
+                                    default: 4
+                                }; 
+                    };
+                    reward = 0;
+                }
+                non-fluents my_test_empty { domain = my_test; }
+                instance my_test_inst { domain = my_test; init-state { a; }; }
+                '''
+        conv = Converter()
+        conv.convert_str(rddl, AG_NAME)
+        _p = conv.world.getState(AG_NAME, 'p', unique=True)
+        self.assertEqual(_p, 0)
+        conv.world.step()
+        p = conv.world.getState(AG_NAME, 'p', unique=True)
+        self.assertEqual(p, _python_switch(_p, {1: 1,
+                                                2: 2,
+                                                3: 3,
+                                                'default': 4}))
+
+    def test_switch_no_case(self):
+        rddl = '''
+                domain my_test {
+                    pvariables { 
+                        p : { state-fluent,  int, default = 0 };
+                        a : { action-fluent, bool, default = false }; 
+                    };
+                    cpfs { p' = switch (p) {
+                                    default : 1
+                                }; 
+                    };
+                    reward = 0;
+                }
+                non-fluents my_test_empty { domain = my_test; }
+                instance my_test_inst { domain = my_test; init-state { a; }; }
+                '''
+        conv = Converter()
+        conv.convert_str(rddl, AG_NAME)
+        p = conv.world.getState(AG_NAME, 'p', unique=True)
+        self.assertEqual(p, 0)
+        conv.world.step()
+        p = conv.world.getState(AG_NAME, 'p', unique=True)
+        self.assertEqual(p, 1)
+
+    def test_switch_cond_op(self):
+        rddl = '''
+                domain my_test {
+                    pvariables { 
+                        p : { state-fluent,  int, default = 0 };
+                        q : { state-fluent,  int, default = 1 };
+                        r : { state-fluent,  int, default = 2 };
+                        s : { state-fluent,  int, default = 3 };
+                        a : { action-fluent, bool, default = false }; 
+                    };
+                    cpfs { p' = switch (q + 2 * r) {
+                                    case q : q,
+                                    case r : r,
+                                    case s : s,
+                                    default: 4
+                                }; 
+                    };
+                    reward = 0;
+                }
+                non-fluents my_test_empty { domain = my_test; }
+                instance my_test_inst { domain = my_test; init-state { a; }; }
+                '''
+        conv = Converter()
+        conv.convert_str(rddl, AG_NAME)
+        p = conv.world.getState(AG_NAME, 'p', unique=True)
+        self.assertEqual(p, 0)
+        conv.world.step()
+        p = conv.world.getState(AG_NAME, 'p', unique=True)
+        q = conv.world.getState(AG_NAME, 'q', unique=True)
+        r = conv.world.getState(AG_NAME, 'r', unique=True)
+        s = conv.world.getState(AG_NAME, 's', unique=True)
+        self.assertEqual(p, _python_switch(q + 2 * r, {q: q,
+                                                       r: r,
+                                                       s: s,
+                                                       'default': 4}))
+
+    def test_switch_case_op(self):
+        rddl = '''
+                domain my_test {
+                    pvariables { 
+                        p : { state-fluent,  int, default = 0 };
+                        q : { state-fluent,  int, default = 1 };
+                        r : { state-fluent,  int, default = 2 };
+                        s : { state-fluent,  int, default = 3 };
+                        a : { action-fluent, bool, default = false }; 
+                    };
+                    cpfs { p' = switch (p) {
+                                    case 2 * q - r : q,
+                                    case r : r,
+                                    case s : s,
+                                    default: 4
+                                }; 
+                    };
+                    reward = 0;
+                }
+                non-fluents my_test_empty { domain = my_test; }
+                instance my_test_inst { domain = my_test; init-state { a; }; }
+                '''
+        conv = Converter()
+        conv.convert_str(rddl, AG_NAME)
+        _p = conv.world.getState(AG_NAME, 'p', unique=True)
+        self.assertEqual(_p, 0)
+        conv.world.step()
+        p = conv.world.getState(AG_NAME, 'p', unique=True)
+        q = conv.world.getState(AG_NAME, 'q', unique=True)
+        r = conv.world.getState(AG_NAME, 'r', unique=True)
+        s = conv.world.getState(AG_NAME, 's', unique=True)
+        self.assertEqual(p, _python_switch(_p, {2 * q - r: q,
+                                                r: r,
+                                                s: s,
+                                                'default': 4}))
+
+    def test_switch_enum(self):
+        rddl = '''
+                domain my_test {
+                    types {
+                        enum_level : {@low, @medium, @high};
+                    };
+                    pvariables { 
+                        p : { state-fluent,  enum_level, default = @low };
+                        q : { state-fluent,  enum_level, default = @medium };
+                        a : { action-fluent, bool, default = false }; 
+                    };
+                    cpfs { p' = switch (q) {
+                                    case @low : @low,
+                                    case @medium : @medium,
+                                    default : @high
+                                }; 
+                    };
+                    reward = 0;
+                }
+                non-fluents my_test_empty { domain = my_test; }
+                instance my_test_inst { domain = my_test; init-state { a; }; }
+                '''
+        conv = Converter()
+        conv.convert_str(rddl, AG_NAME)
+        _p = conv.world.getState(AG_NAME, 'p', unique=True)
+        self.assertEqual(_p, 'low')
+        conv.world.step()
+        p = conv.world.getState(AG_NAME, 'p', unique=True)
+        q = conv.world.getState(AG_NAME, 'q', unique=True)
+        self.assertEqual(p, _python_switch(q, {'low': 'low',
+                                               'medium': 'medium',
+                                               'default': 'high'}))
+
+    def test_switch_no_default(self):
+        rddl = '''
+                domain my_test {
+                    pvariables { 
+                        p : { state-fluent,  int, default = 0 };
+                        a : { action-fluent, bool, default = false }; 
+                    };
+                    cpfs { p' = switch (p) {
+                                    case 2 : 1
+                                }; 
+                    };
+                    reward = 0;
+                }
+                non-fluents my_test_empty { domain = my_test; }
+                instance my_test_inst { domain = my_test; init-state { a; }; }
+                '''
+        conv = Converter()
+        with self.assertRaises(AssertionError):
+            conv.convert_str(rddl, AG_NAME)
+
+    def test_switch_multiple_default(self):
+        rddl = '''
+                domain my_test {
+                    pvariables { 
+                        p : { state-fluent,  int, default = 0 };
+                        a : { action-fluent, bool, default = false }; 
+                    };
+                    cpfs { p' = switch (p) {
+                                    default: 3,
+                                    case 2 : 1,
+                                    default: 2
+                                }; 
+                    };
+                    reward = 0;
+                }
+                non-fluents my_test_empty { domain = my_test; }
+                instance my_test_inst { domain = my_test; init-state { a; }; }
+                '''
+        conv = Converter()
+        with self.assertRaises(AssertionError):
+            conv.convert_str(rddl, AG_NAME)
 
     def test_switch_vars_consts_enum(self):
         rddl = '''
